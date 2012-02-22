ed0db7329917b8a3e90a9b71f05f8e0c68f0bb68
Joseph Southern <joe@NervousYak.com>
2012-02-21 23:03:52 -0600

    fixed header javascript issue

--- app/assets/javascripts/_header.js
+++ app/assets/javascripts/_header.js
@@ -6,18 +6,27 @@ $(document).ready(function(){
 	});
 }); // close document.ready
 
+var closing = false;
+
 function showHeaderMenu(){  
+							closing = false;
 							var menu = $(".header_menu_move");
 							menu.animate({"margin-top": "+=2em"}, 
 										  "slow",
 										   function(){
-												    menu.css("z-index", "100");	
+													if (closing == false)
+												    	menu.css("z-index", "100");	
 											}
 					                    );
 };
 function hideHeaderMenu(){  
+						closing = true;
 	 					var menu = $(".header_menu_move");
 						menu.css("z-index", -1);
-						menu.animate({"margin-top": "-2em"}, "slow");
+						menu.animate({"margin-top": "-2em"}, 
+							"slow", 
+							function(){
+								menu.css("z-index", -1);
+							});
 					};
 					
